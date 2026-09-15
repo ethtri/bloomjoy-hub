e2c51cda84b919abac01041f0b18263b912bfc39
ethtri <ethtri@gmail.com>
2026-09-14 19:53:52 -0700

    test(refunds): bind legacy cash fixtures to trusted amounts

--- supabase/tests/refund_cash_completion_safety.sql
+++ supabase/tests/refund_cash_completion_safety.sql
@@ -293,7 +293,7 @@ select is(
     public.service_complete_cash_refund_as_actor(
       '75000000-0000-4000-8000-000000000001',
       '75600000-0000-4000-8000-000000000002',
-      null, null, null, null, null, null
+      900, null, null, null, null, null
     ) ->> 'updateApplied'
   )::boolean,
   true,
@@ -305,7 +305,7 @@ select is(
     public.service_complete_cash_refund_as_actor(
       '75000000-0000-4000-8000-000000000001',
       '75600000-0000-4000-8000-000000000003',
-      null, null, null, null, null, null
+      700, null, null, null, null, null
     ) ->> 'updateApplied'
   )::boolean,
   true,
